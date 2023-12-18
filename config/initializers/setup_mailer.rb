ff5d265f5c2081f6a0f31999f472ca03e22daba8
Bhaskar <bhaskarshines11@gmail.com>
2023-12-18 20:23:30 +0530

    styled devise pages and tried to add addto acrt button on product

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,13 +1,13 @@
-ActionMailer::Base.smtp_settings=
-{
+ActionMailer::Base.smtp_settings =
+  {
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
-    :authentication =>"plain",
-    :enable_starttls_auto =>true,
+    :authentication => "plain",
+    :enable_starttls_auto => true,
     :user_name => "bhaskaryerrolla99@gmail.com",
     :password => "hefvvuzoqmtgjwgs",
-    :openssl_verify_mode => 'none',
+    :openssl_verify_mode => "none",
     :open_timeout => 5,
-    :read_timeout => 5 
-}
+    :read_timeout => 5,
+  }
