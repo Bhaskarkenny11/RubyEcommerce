6530d2d0e478f647f79071e5ebf9f9008bdca720
Bhaskar <bhaskarshines11@gmail.com>
2023-12-26 20:30:48 +0530

    undo action

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_064118) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_26_132953) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_064118) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -82,6 +82,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_064118) do
     t.integer "address_id"
   end
 
+  create_table "post_versions", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_versions_on_post_id"
+  end
+
+  create_table "product_versions", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_versions_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "descrption"
@@ -94,6 +110,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_064118) do
     t.datetime "updated_at", null: false
     t.integer "sub_category_id"
     t.string "Product_code"
+    t.string "image"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -131,4 +148,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_064118) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "product_versions", "products"
 end
