d6a997fc043b396188b8d4c2bb640ec9185201d3
Bhaskar <bhaskarshines11@gmail.com>
2023-12-26 20:40:11 +0530

    commited changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_132953) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_132953) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -90,14 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_132953) do
     t.index ["post_id"], name: "index_post_versions_on_post_id"
   end
 
-  create_table "product_versions", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_product_versions_on_product_id"
-  end
-
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "descrption"
@@ -148,5 +140,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_132953) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "product_versions", "products"
+  add_foreign_key "post_versions", "posts"
 end
