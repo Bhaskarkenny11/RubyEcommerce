3df2b48817b5252493729255937669ae4087e44e
Bhaskar <bhaskarshines11@gmail.com>
2023-12-27 00:26:13 +0530

    deleted spaces

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,33 @@
 class OrdersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def create 
-        #binding.pry
-        @order =Order.new(order_params)
-        @order.user_id=current_user.id
-        @order.address_id = params[:order][:address]
-         # @order.order_date=Date.today
-        # @order.total_amount=1             #before_validation method in model 
-        # @order.order_number="ybk#{Random.rand(100)}"
-        #binding.pry
-        if @order.save
-         redirect_to order_path(@order.id), notice: "your order has been created"
-        end
-        
+  def create
+    #binding.pry
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.address_id = params[:order][:address]
+    # @order.order_date=Date.today
+    # @order.total_amount=1             #before_validation method in model
+    # @order.order_number="ybk#{Random.rand(100)}"
+    #binding.pry
+    if @order.save
+      redirect_to order_path(@order.id), notice: "your order has been created"
     end
+  end
 
-    def show 
-        @order=Order.find(params[:id])
-    end
-    
+  def show
+    @order = Order.find(params[:id])
+  end
 
-        def index
-         
-            @orders=Order.all
-            @user=current_user
-            @orders = current_user.orders.where("strftime('%m', created_at) = ?", params[:month].to_i.to_s)
+  def index
+    @orders = Order.all
+    @user = current_user
+    @orders = current_user.orders.where("strftime('%m', created_at) = ?", params[:month].to_i.to_s)
+  end
 
-        end
+  private
 
-
-
-    private
-    def order_params
-        #params[:order].permit()
-    end
+  def order_params
+    #params[:order].permit()
+  end
 end
