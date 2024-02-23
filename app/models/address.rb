41d74ad8dc79e7d079a8f2fcd082aa4fbd722b65
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-23 18:48:20 +0530

    added address fields to be mandatory in address model,and in address_form in addr_view_pages removed dropdown for numfield and added read-only

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,7 @@ class Address < ApplicationRecord
     belongs_to :user
     has_many :orders
 
+    validates_presence_of :city,:street
     def full_address
         "#{self.street} - #{self.city}" 
     end
